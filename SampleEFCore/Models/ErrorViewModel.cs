4d89f965339da176198f0a4ddc1abf135935f56f
rtfmpliz <rtfmpliz@gmail.com>
2018-06-28 13:05:02 +0700

    Initialization

---
+++ SampleEFCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SampleEFCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
